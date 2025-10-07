2978aaff3640c5a49c98a3ee2afe75ce98c1e4fe
AngeloDavideGe <adgenovese2@gmail.com>
2025-10-07 16:16:00 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -2,19 +2,26 @@ using Microsoft.EntityFrameworkCore;
 using Data.ApplicationDbContext;
 using Views.DropboxSettings;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplicationOptions options = new WebApplicationOptions
+{
+    ContentRootPath = Directory.GetCurrentDirectory(),
+    WebRootPath = "wwwroot"
+};
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(options);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.Configure<DropboxSettings>(builder.Configuration.GetSection("Dropbox"));
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGenWithSecurity(); // Metodo custom per configurare Swagger con sicurezza
 
-builder.Services.AddCors(options =>
+builder.Services.AddCors(corsOptions =>
 {
-    options.AddDefaultPolicy(policy =>
+    corsOptions.AddDefaultPolicy(policy =>
     {
         policy
             .AllowAnyOrigin()
@@ -31,11 +38,28 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors();
-app.UseApiKeyAndAuthorizationValidation(); // Metodo custom per validare API Key e Authorization
-app.UseHttpsRedirection();
-app.UseAuthorization();
+string configSourcePath = Path.Combine(builder.Environment.ContentRootPath, "Configs", "config.json");
+string configTargetPath = Path.Combine(builder.Environment.WebRootPath, "config.runtime.json");
 
+if (File.Exists(configSourcePath))
+{
+    File.Copy(configSourcePath, configTargetPath, overwrite: true);
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCors();
+app.UseApiKeyAndAuthorizationValidation();
+
+// app.UseWhen(
+//     context =>
+//         !context.Request.Path.StartsWithSegments("/config.runtime.json"),
+//     appBuilder =>
+//     {
+//         appBuilder.UseApiKeyAndAuthorizationValidation();
+//     });
+
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
